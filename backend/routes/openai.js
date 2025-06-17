b7cdd3148f79253a2c630e864537bbd99b74ac09
lucar <lucarizz@umich.edu>
2025-06-17 18:02:11 -0400

    speech to text endpoint

--- backend/routes/openai.js
+++ backend/routes/openai.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const { OpenAI } = require('openai')
 const dotenv = require('dotenv')
+const multer = require('multer') // handles file uploads
+const fs = require('fs') // imports nodes built in file system module
 const router = express.Router()
+
+const uploads = multer({dest: '/uploads'})
 
 dotenv.config()
 
@@ -10,6 +14,8 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+
+// text to text
 router.post('/chat', async (req, res) => {
     const { chat } = req.body
     if (!chat) {
@@ -31,5 +37,48 @@ router.post('/chat', async (req, res) => {
         res.status(500).json({ err: 'Failed to fetch reply from OpenAI.' });
     }
 })
+
+// speech to text
+router.post('speech-to-text', uploads.single('audio'), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({error: 'No audio file found'})
+    }
+
+    try {
+        const audioText = fs.readFileSync(req.file.path)
+        const transcription = await openai.audio.transcriptions.create({
+            file: audioText,
+            model: 'whisper-1',
+            response_format: 'json'
+        });
+
+        const userMessage = transcription.text
+        console.log('Transcribed text:', userMessage);
+
+        const chatCompletion = await openai.chat.completions.create({
+            model: 'gpt-4o',
+            messages: [
+                { role: 'system', content: 'You are a friendly and supportive wellness coach for University of Michigan students. Keep content brief and talk like a therapist, so ask questions to diver deeper and have a conversation to help' },
+                { role: 'user', content: userMessage }
+            ]
+        });
+
+        const reply = chatCompletion.choices[0].message.content;
+        
+        res.json(reply)
+        fs.unlinkSync(req.file.path);
+    }
+    catch(err) {
+        console.error('Error in speech-to-speech:', error);
+        res.status(500).json({ error: 'Failed to process speech-to-speech request.' });
+    }
+})
+
+
+
+
+
+
+
 
 module.exports = router;
